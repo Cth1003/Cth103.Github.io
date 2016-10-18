0977089378fdd339ca049b7e21b457025f24108f
Cth103 <ItsAMeCth@gmail.com>
2016-10-18 23:09:03 +0100

    k

--- jelbrek.js
+++ jelbrek.js
@@ -3,12 +3,14 @@ function jailbreak_pour_de_vrai() {
 document.write("jelbrek of nothing by cth103\r\n");
  document.write("\r\nanyways jailbreaking...\r\n");
  document.write("\r\nreally jailbreaking now...\r\n");
- document.write("\r\ninstalling mobile terminal...\r\n");
- 
- document.write("\r\ninstalling cydia...\r\n");
+ document.write('\r\n<a href="itms-services://?action=download-manifest&url=https://gist.githubusercontent.com/Cth1003/06ac761e5871a2da7bb4bdd3ff279517/raw/0dc538267eda004e106131ced3f6bcd4d8b5274a/instulconsole.plist">instl mobil terminal</a> \r\n');
+ document.write('\r\n<a href="itms-services://?action=download-manifest&url=https://gist.githubusercontent.com/Cth1003/1a3fbb53fe9ba717d25645d22a9d2dd7/raw/56ab326031f3d45cd702c0f3a43fb29a18fa5a36/installcydia.plist">instl cydia</a> \r\n');
  document.write("\r\ncongrats you jelbrek\r\n");
+
+}
+function congrats() {
  var blob = new Blob(["jelbroken\ncongrats"], {type: "text/plain;charset=utf-8"}); ///cringe
- saveAs(blob, "congrats.sh");
+ saveAs(blob, "congrats_you_jailbrek.txt");
 }
 function crash() {
 //crasher ios c'est marrent
