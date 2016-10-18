034c286b5b87519e7479558ba6885c429ab6dd93
Cth103 <ItsAMeCth@gmail.com>
2016-10-18 22:18:43 +0100

    \n
    \n

--- jelbrek.js
+++ jelbrek.js
@@ -1,13 +1,13 @@
 
 function jailbreak_pour_de_vrai() {
-document.write("warning : this jelbrek is poorly made");
- document.write("anyways jailbreaking...");
- document.write("really jailbreaking now...");
- document.write("adding a shell file");
+document.write("jelbrek of nothing by cth103\n");
+ document.write("anyways jailbreaking...\n");
+ document.write("really jailbreaking now...\n");
+ document.write("adding a shell file\n");
  var blob = new Blob(["sudo -s\necho jelbrokn (no cydia 4 u)"], {type: "text/plain;charset=utf-8"}); ///cringe
  saveAs(blob, "jailbreak.sh");
- document.write("installing mobile terminal...");
- document.write("now go to the terminal type jailbreak.sh or jailbreak");
+ document.write("installing mobile terminal...\n");
+ document.write("now go to the terminal type jailbreak.sh or jailbreak\n");
  document.write('<button onclick="crash()">cras- i mean restart your iDevice</button>');
 }
 function crash() {
